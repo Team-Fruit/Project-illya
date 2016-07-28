39a333b641c8ae9cb0f17401380d6739b01448f2
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-07-29 01:30:03 +0900

    Ore Generation #4

--- src/main/java/com/bebehp/mc/projectillya/block/ModOreGenerator.java
+++ src/main/java/com/bebehp/mc/projectillya/block/ModOreGenerator.java
@@ -10,7 +10,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 
-public class ModOreGenerator implements IWorldGenerator {
+public class ModOreGenerator implements IWorldGenerator{
 
 	@Override
 	public void generate(final Random random, final int chunkX, final int chunkZ, final World world, final IChunkProvider chunkGenerator,final IChunkProvider chunkProvider) {
@@ -27,38 +27,39 @@ public class ModOreGenerator implements IWorldGenerator {
 	}
 
 	private void generateOreRuby(final World world, final Random random, final int x, final int z) {
-		for (int i = 0; i < 20; i++) {
-			final int genX = x + random.nextInt(16);
-			final int genY = random.nextInt(20);
-			final int genZ = z + random.nextInt(16);
-			new WorldGenMinable(ModBlocks.ore, 0, 1, Blocks.stone).generate(world, random, genX, genY, genZ);
+		for (int i = 0; i < 5; i++) {
+			final int rubyX = x + random.nextInt(16);
+			final int rubyY = random.nextInt(20);
+			final int rubyZ = z + random.nextInt(16);
+			new WorldGenMinable(ModBlocks.ore, 0, 3, Blocks.stone).generate(world, random, rubyX, rubyY, rubyZ);
 		}
 	}
 
 	private void generateOreSapphire(final World world, final Random random, final int x, final int z) {
-		for (int i = 0; i < 20; i++) {
-			final int genX = x + random.nextInt(16);
-			final int genY = random.nextInt(20);
-			final int genZ = z + random.nextInt(16);
-			new WorldGenMinable(ModBlocks.ore, 1, 1, Blocks.stone).generate(world, random, genX, genY, genZ);
+		for (int i = 0; i < 5; i++) {
+			final int sapphireX = x + random.nextInt(16);
+			final int sapphireY = random.nextInt(20);
+			final int sapphireZ = z + random.nextInt(16);
+			new WorldGenMinable(ModBlocks.ore, 1, 3, Blocks.stone).generate(world, random, sapphireX, sapphireY, sapphireZ);
 		}
 	}
 
 	private void generateOrePeridot(final World world, final Random random, final int x, final int z) {
-		for (int i = 0; i < 80; i++) {
-			final int genX = x + random.nextInt(16);
-			final int genY = random.nextInt(32);
-			final int genZ = z + random.nextInt(16);
-			new WorldGenMinable(ModBlocks.ore, 2, 5, Blocks.stone).generate(world, random, genX, genY, genZ);
+		for (int i = 0; i < 10; i++) {
+			final int peridotX = x + random.nextInt(16);
+			final int peridotY = random.nextInt(32);
+			final int peridotZ = z + random.nextInt(16);
+			new WorldGenMinable(ModBlocks.ore, 2, 3, Blocks.stone).generate(world, random, peridotX, peridotY, peridotZ);
 		}
 	}
 
 	private void generateOreTopaz(final World world, final Random random, final int x, final int z) {
-		for (int i = 0; i < 80; i++) {
-			final int genX = x + random.nextInt(16);
-			final int genY = random.nextInt(32);
-			final int genZ = z + random.nextInt(16);
-			new WorldGenMinable(ModBlocks.ore, 3, 5, Blocks.stone).generate(world, random, genX, genY, genZ);
+		for (int i = 0; i < 10; i++) {
+			final int topazX = x + random.nextInt(16);
+			final int topazY = random.nextInt(32);
+			final int topazZ = z + random.nextInt(16);
+			new WorldGenMinable(ModBlocks.ore, 3, 3, Blocks.stone).generate(world, random, topazX, topazY, topazZ);
 		}
 	}
+
 }
--- src/main/java/com/bebehp/mc/projectillya/reference/Reference.java
+++ src/main/java/com/bebehp/mc/projectillya/reference/Reference.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
 	public static final String MODID = "Project-illya";
 	public static final String NAME = "Project-illya";
-	public static final String VERSION = "0.0.1.0pre-alpha";
+	public static final String VERSION = "0.0.4.3pre-alpha";
 	public static final String FORGE = "${forgeversion}";
 	public static final String MINECRAFT = "${mcversion}";
 	public static final String PROXY_SERVER = "com.bebehp.mc.projectillya.proxy.CommonProxy";
